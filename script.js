26fb02e4a5c70e8d4577a82a753b937f0f5382e9
cdROma <truemrv@gmail.com>
2018-10-10 10:40:23 +0300

    ok

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var ads = document.getElementById('ads_left');
 
 ads.addEventListener("DOMNodeInserted", function() {
-    ads.innerHTML = "<h3 id='ads' style='z-index: 9999; color: #99b1c6; text-align: center;'>Тут была их реклама</h3>";
+    ads.innerHTML = "<h3 id='ads' style='z-index: 9999; color: #99b1c6;'>Тут была их реклама</h3>";
     }
 );
