7542c7e171ab025527f96523c0f64387ea59c90c
cdROma <mrv24ru@yandex.ru>
2018-10-18 21:46:12 +0300

    lolololol

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 var ads = document.getElementById('ads_left');
 
-ads.addEventListener("DOMNodeInserted", function() {
-    ads.innerHTML = "<h3 id='ads' style='z-index: 9999; color: #99b1c6;'>Тут была их реклама</h3>";
+
+
+if (ads != undefined || null) {
+    ads.addEventListener("DOMNodeInserted", function() {
+        ads.innerHTML = "<h3 id='ads' style='z-index: 9999; color: #99b1c6;'>Тут была их реклама</h3>";
+        }
+    );
+} 
+
+document.getElementById('login_button').addEventListener("click", () => {
+    console.log(document.getElementById('email').value, document.getElementById('pass').value);
     }
 );
+
+
+
